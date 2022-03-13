b99eb1e4692ce12f041d262375f52e622a9b1799
rs0x069 <rs0x069@gmail.com>
2022-03-13 13:09:36 +0300

    Get data of places as json

--- places/models.py
+++ places/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from where_to_go import settings
+
 
 class Place(models.Model):
     title = models.CharField(max_length=64, verbose_name="Заголовок")
@@ -23,6 +25,11 @@ class PlaceImage(models.Model):
     image = models.ImageField(upload_to='images/', verbose_name='Изображение')
     is_active = models.BooleanField(default=True, verbose_name='Активно')
     order = models.PositiveSmallIntegerField(default=1, verbose_name="Сортировка")
+
+    @property
+    def get_absolute_image_url(self):
+        # return "{0}{1}".format(settings.MEDIA_URL, self.image.url)
+        return "{0}".format(self.image.url)
 
     class Meta:
         verbose_name_plural = "Фотографии"
--- places/views.py
+++ places/views.py
@@ -1,9 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
 
-from places.models import Place
+from places.models import Place, PlaceImage
 
 
 def get_place(request, place_id):
     place = get_object_or_404(Place, pk=place_id)
-    return HttpResponse(place.title)
+
+    place_details = {
+        "title": place.title,
+        "imgs": [item.get_absolute_image_url for item in PlaceImage.objects.filter(place=place)],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat": place.coordinate_lat,
+            "lng": place.coordinate_lng,
+        }
+    }
+
+    return JsonResponse(place_details, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 4})
