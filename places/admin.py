729ac9b7d6ccfcc4b40ea4b39d029d4cd01cf0a9
rs0x069 <rs0x069@gmail.com>
2022-03-13 21:34:13 +0300

    Preview photo in admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
 from .models import Place, PlaceImage
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
-    fields = ('image', 'order')
+    fields = ('image', 'preview_image', 'order')
     ordering = ('order',)
     extra = 1
+    readonly_fields = ('preview_image',)
+
+    def preview_image(self, instance):
+        return format_html('<img src="{}" height={} />', instance.image.url, '200px')
+    preview_image.short_description = "Предосмотр"
 
 
 @admin.register(Place)
