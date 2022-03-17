51780124e841b628a56091bb48562b470c1b9af9
rs0x069 <rs0x069@gmail.com>
2022-03-17 20:45:35 +0300

    Load places images from url

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,8 +1,11 @@
+from urllib.parse import urlparse
+
 import requests
 import json
 
 from django.core.management.base import BaseCommand, CommandError
-from places.models import Place
+from django.core.files.base import ContentFile
+from places.models import Place, PlaceImage
 
 
 class Command(BaseCommand):
@@ -21,15 +24,27 @@ class Command(BaseCommand):
         else:
             if response.status_code == 200:
                 data = json.dumps(response.json())
-                places_json = json.loads(data)
+                place_data = json.loads(data)
 
                 obj, is_created = Place.objects.get_or_create(
-                    title=places_json['title'],
-                    description_short=places_json['description_short'],
-                    description_long=places_json['description_long'],
-                    coordinate_lat=places_json['coordinates']['lat'],
-                    coordinate_lng=places_json['coordinates']['lng'],
+                    title=place_data['title'],
+                    description_short=place_data['description_short'],
+                    description_long=place_data['description_long'],
+                    coordinate_lat=place_data['coordinates']['lat'],
+                    coordinate_lng=place_data['coordinates']['lng'],
                 )
 
                 if not is_created:
                     print("Place doesn't created. The place '%s' is exists" % obj)
+                else:
+                    images_url = list(place_data['imgs'])
+
+                    for item in images_url:
+                        image_filename = urlparse(item).path.split('/')[-1]
+                        image_response = requests.get(item)
+
+                        if image_response.status_code == 200:
+                            place_image = PlaceImage()
+                            place_image.place = obj
+                            place_image.image.save(image_filename, ContentFile(image_response.content), save=False)
+                            place_image.save()
