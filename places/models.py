e864ef148f871a9bed9cd2b558d050646b4c844c
rs0x069 <rs0x069@gmail.com>
2022-03-22 22:56:53 +0300

    Allow to leave empty description_short and description_long fields

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(max_length=64, verbose_name="Заголовок", unique=True)
-    description_short = models.TextField(verbose_name="Короткое описание")
-    description_long = HTMLField(verbose_name="Полное описание")
+    description_short = models.TextField(blank=True, verbose_name="Короткое описание")
+    description_long = HTMLField(blank=True, verbose_name="Полное описание")
     coordinate_lat = models.DecimalField(max_digits=17, decimal_places=15, verbose_name="Координаты: широта",
                                          help_text="latitude, lat")
     coordinate_lng = models.DecimalField(max_digits=17, decimal_places=15, verbose_name="Координаты: долгота",
