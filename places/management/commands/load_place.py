04df3e61153fa2822fd3b2984c76a8a8d53f5544
rs0x069 <rs0x069@gmail.com>
2022-03-24 20:27:04 +0300

    Refactoring

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,9 +1,9 @@
-import json
+import os
 import requests
 
 from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
-from urllib.parse import urlparse
+from urllib.parse import urlparse, unquote
 
 from places.models import Place, PlaceImage
 
@@ -17,34 +17,32 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         url = options['url']
 
-        try:
-            response = requests.get(url)
-        except requests.RequestException as err:
-            print(err)
-        else:
-            if response.status_code == 200:
-                data = json.dumps(response.json())
-                place_data = json.loads(data)
+        response = requests.get(url)
+        response.raise_for_status()
 
-                obj, is_created = Place.objects.get_or_create(
-                    title=place_data['title'],
-                    description_short=place_data['description_short'],
-                    description_long=place_data['description_long'],
-                    coordinate_lat=place_data['coordinates']['lat'],
-                    coordinate_lng=place_data['coordinates']['lng'],
-                )
+        place_content = response.json()
 
-                if not is_created:
-                    print("Place doesn't created. The place '%s' is exists" % obj)
-                else:
-                    images_url = list(place_data['imgs'])
+        place_created, is_place_created = Place.objects.get_or_create(
+            title=place_content['title'],
+            defaults={
+                'description_short': place_content['description_short'],
+                'description_long': place_content['description_long'],
+                'coordinate_lat': place_content['coordinates']['lat'],
+                'coordinate_lng': place_content['coordinates']['lng'],
+            }
+        )
 
-                    for item in images_url:
-                        image_filename = urlparse(item).path.split('/')[-1]
-                        image_response = requests.get(item)
+        if not is_place_created:
+            print("Place doesn't created. The place '%s' is exists" % place_created)
+            return False
 
-                        if image_response.status_code == 200:
-                            place_image = PlaceImage()
-                            place_image.place = obj
-                            place_image.image.save(image_filename, ContentFile(image_response.content), save=False)
-                            place_image.save()
+        images_url = place_content['imgs']
+        for image_url in images_url:
+            image_path = urlparse(image_url).path
+            image_filename = unquote(os.path.basename(image_path))
+            image_response = requests.get(image_url)
+            image_response.raise_for_status()
+
+            place_image = PlaceImage(place=place_created)
+            place_image.image.save(image_filename, ContentFile(image_response.content), save=False)
+            place_image.save()
