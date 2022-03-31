8333f54ea6c8999d5b7bb050628d6357d0687d56
rs0x069 <rs0x069@gmail.com>
2022-03-31 21:37:24 +0300

    Change connecting MEDIA_ROOT

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('places/', include('places.urls')),
     path('tinymce/', include('tinymce.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
